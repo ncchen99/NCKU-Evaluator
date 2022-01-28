a1e0a74a9055d8c879cc0c4920af1daa278a2b2c
ncchen99 <ncchen99@gmail.com>
2022-01-28 21:25:23 +0800

    👃🏿

--- content.js
+++ content.js
@@ -1,6 +1,7 @@
 console.log("🤖🆙");
 
 if ($("table.table").length > 0) {
+  $("table.table > thead  > tr > th:nth-child(7)")[0].style.width = "11%";
   $("table.table > tbody  > tr:visible").each(function (trIdx, tr) {
     $(tr)
       .find("td")
@@ -8,34 +9,29 @@ if ($("table.table").length > 0) {
         if (tdIdx == 6) {
           $(td).removeClass("sm"); //.addClass("ui segment").css("position", "list-item");
           td.innerHTML = `
-          <div class="main ui container">
-          <div class="ui tab active">
-            <div class="ui button" id="${trIdx}">
+            <div class="medium fluid ui button my-button" id="button${trIdx}">
              ${td.innerHTML}
             </div>
-            <div class="ui popup transition hidden" id="${
-              trIdx + 1000
-            }">
-              <div class="header">User Rating</div>
-                <div class="content"><div class="ui star rating"><i class="active icon"></i><i class="active icon"></i><i class="active icon"></i><i class="active icon"></i><i class="active icon"></i></div>
-              </div>
-            </div>
-          </div>
-          </div>`;
-          $(`.button#${trIdx}`).popup({
-            popup: $(`.popup#${trIdx + 1000}`),
-            on: "hover",
-            inline:true,
-            minWidth:50,
-          });
-          td.addEventListener(
-            "mouseover",
-            function (event) {
+            <div class="ui popup flowing bottom left transition hidden my-popup" id="popup${trIdx}">
+            <div class='header'>User Rating</div><div class='content'><div class='ui star rating'><i class='active icon'></i><i class='active icon'></i><i class='active icon'></i><i class='active icon'></i><i class='active icon'></i></div></div>
+            </div>`;
+          $(`.button#button${trIdx}`)
+            .mouseenter(function () {
               // highlight the mouseover target
-              $(`.popup#${trIdx + 1000}`).removeClass("hidden").css("position","absolute");
-            },
-            false
-          );
+              var rect = $(`.button#button${trIdx}`)[0].getBoundingClientRect();
+              $(`.popup#popup${trIdx}`)
+                .removeClass("hidden")
+                .addClass("visible");
+              $(`.popup#popup${trIdx}`)
+                .css("top", rect.bottom - document.body.getBoundingClientRect().top)
+                .css("left",rect.left); //
+            })
+            .mouseleave(function () {
+              // highlight the mouseover target
+              $(`.popup#popup${trIdx}`)
+                .removeClass("visible")
+                .addClass("hidden");
+            });
         }
       });
   });
