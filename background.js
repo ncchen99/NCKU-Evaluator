c0e32ded762c307fc9348362e17b8fcf31a8db72
ncchen99 <ncchen99@gmail.com>
2022-03-01 19:01:45 +0800

    🇲🇴

--- background.js
+++ background.js
@@ -1,17 +1,63 @@
-//// background.js ////
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  // 2. A page requested user data, respond with a copy of `user`
-  if (message.name == "get-prof-data") {
-    fetch(`https://ncchen.ga/ncku-evaluation/data/urschool/${message.prof}.json`)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (result) {
-        // console.log(result);
-        sendResponse(result);
-      }).catch(function(e) {
-        console.log(e); // "oh, no!"
-      });
+async function get_data(url, type) {
+  let res = await fetch(url);
+  if (!res.ok) {
+    console.log(`NETWORK ERROR: ${res.status}`);
+    return { err: res.status };
   }
+  return type == "json" ? await res.json() : await res.text();
+}
+
+var json_data = {};
+const resource_list = ["nckuhub", "urschool"];
+
+chrome.runtime.onMessage.addListener(async function (
+  request,
+  sender,
+  sendResponse
+) {
+  if (request.method == "get_data") {
+    resource_list.forEach(async (item) => {
+      let data = await get_data(
+        `https://ncchen.ga/ncku-evaluation/data/${item}-sha256.txt`,
+        "text"
+      );
+      var sha = `${item}-sha256`;
+      var item_bk = item;
+      console.log(`sha:${sha}`);
+      chrome.storage.local.get([sha], async function (result) {
+        if (result[sha] != data) {
+          console.log("Detected new data!");
+          // Cookies.set(`${item}-sha256`, data, { expires: 30 });
+          chrome.storage.local.set({ [sha]: data });
+          json_data[item_bk] = await get_data(
+            `https://ncchen.ga/ncku-evaluation/data/${item_bk}.json`,
+            "json"
+          );
+          chrome.storage.local.set({
+            [item_bk]: json_data[item_bk],
+          });
+        } else {
+          chrome.storage.local.get([item_bk], function (result) {
+            json_data[item_bk] = result[item_bk];
+            console.log(
+              "Returning exist data： " + JSON.stringify(json_data[item_bk])
+            );
+          });
+        }
+      });
+    });
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        method: "response_data",
+        json_data: json_data,
+      });
+    });
+    sendResponse({ complete: "ok" });
+    // return true from the event listener to indicate you wish to send a response asynchronously
+    // (this will keep the message channel open to the other end until sendResponse is called).
+    // console.log("background get data event");
+  } else {
+    sendResponse({});
+  } // snub them.
+  return true;
 });
--- content.js
+++ content.js
@@ -1,33 +1,8 @@
 console.log("🤖🆙");
 //// content.js ////
-if (
-  $("table.table").length > 0 &&
-  $("table.table > thead  > tr > th:nth-child(7)").html().includes("教師姓名")
-) {
-  // TODO：　add table filter
-  $("table.table > thead  > tr > th:nth-child(7)")[0].style.width = "11%";
-  $("table.table > tbody  > tr:visible").each(function (trIdx, tr) {
-    $(tr)
-      .find("td")
-      .each(async function (tdIdx, td) {
-        if (tdIdx == 6) {
-          $(td).removeClass("sm"); //.addClass("ui segment").css("position", "list-item");
-          var profs_data = [];
-          td.innerHTML
-            .replace("*", "")
-            .split("<br>")
-            .forEach((value) => {
-              fetch(
-                `https://ncchen.ga/ncku-evaluation/data/urschool/${value}.json`
-              )
-                .then((response) => response.json())
-                .then((data) => (profs_data[value] = data))
-                .catch((e) => {
-                  profs_data[value] = "找不到資料耶🥺";
-                });
-            });
-          console.log(profs_data);
-          td.innerHTML = `
+
+function display_data(profs_data, td, trIdx) {
+  td.innerHTML = `
             <div class="medium fluid ui button my-button" id="button${trIdx}">
              ${td.innerHTML}
             </div>
@@ -36,27 +11,71 @@ if (
               profs_data
             )}<div class='content'><div class='ui star rating'><i class='active icon'></i><i class='active icon'></i><i class='active icon'></i><i class='active icon'></i><i class='active icon'></i></div></div>
             </div>`;
-          $(`.button#button${trIdx}`)
-            .mouseenter(function () {
-              // highlight the mouseover target
-              var rect = $(`.button#button${trIdx}`)[0].getBoundingClientRect();
-              $(`.popup#popup${trIdx}`)
-                .removeClass("hidden")
-                .addClass("visible");
-              $(`.popup#popup${trIdx}`)
-                .css(
-                  "top",
-                  rect.bottom - document.body.getBoundingClientRect().top
-                )
-                .css("left", rect.left); //
-            })
-            .mouseleave(function () {
-              // highlight the mouseover target
-              $(`.popup#popup${trIdx}`)
-                .removeClass("visible")
-                .addClass("hidden");
+  $(`.button#button${trIdx}`)
+    .mouseenter(function () {
+      // highlight the mouseover target
+      var rect = $(`.button#button${trIdx}`)[0].getBoundingClientRect();
+      $(`.popup#popup${trIdx}`).removeClass("hidden").addClass("visible");
+      $(`.popup#popup${trIdx}`)
+        .css("top", rect.bottom - document.body.getBoundingClientRect().top)
+        .css("left", rect.left); //
+    })
+    .mouseleave(function () {
+      // highlight the mouseover target
+      $(`.popup#popup${trIdx}`).removeClass("visible").addClass("hidden");
+    });
+}
+function filter_data(json_data) {
+  console.log(json_data);
+  $("table.table > thead  > tr > th:nth-child(7)")[0].style.width = "11%";
+  // var profs_set = new Set();
+  $("table.table > tbody  > tr:visible").each(function (trIdx, tr) {
+    $(tr)
+      .find("td")
+      .each(function (tdIdx, td) {
+        if (tdIdx == 6) {
+          var profs_data = {};
+          $(td).removeClass("sm"); //.addClass("ui segment").css("position", "list-item");
+          td.innerHTML
+            .replace("*", "")
+            .split("<br>")
+            .forEach((value) => {
+              if (value in json_data["urschool"]["professors"]) {
+                profs_data[value] = json_data["urschool"]["professors"][value];
+              } else {
+                profs_data[value] = [{ res: "找不到資料耶🥺" }];
+              }
+              console.log("fetching:" + value);
             });
+          display_data(profs_data, td, trIdx);
         }
       });
+  });
+}
+
+var json_data = {};
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log("received");
+  if (request.method == "response_data") {
+    if (request.json_data) {
+      json_data = request.json_data;
+      filter_data(json_data);
+      sendResponse({ complete: "ok" });
+    }
+  }
+  return true;
+});
+
+if (
+  $("table.table").length > 0 &&
+  $("table.table > thead  > tr > th:nth-child(7)").html().includes("教師姓名")
+) {
+  chrome.runtime.sendMessage({ method: "get_data" }, function (response) {
+    if (response.complete == "ok") {
+      console.log("👌👋");
+    } else {
+      console.log("📛");
+    }
   });
 }
