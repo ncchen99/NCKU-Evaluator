c672b9409d23f2ea1a886f911be4315c256c63ac
ncchen99 <ncchen99@gmail.com>
2022-03-06 02:52:41 +0800

    🐸

--- content.js
+++ content.js
@@ -1,4 +1,5 @@
 console.log("🤖🆙");
+window.json_data = {};
 //// content.js ////
 function formatter(type, data) {
   var content = "";
@@ -21,22 +22,18 @@ function formatter(type, data) {
     ];
     for (const key in data) {
       if (Array.isArray(data[key][0])) {
-        if (Object.keys(data).length != 1)
-          content += `<div class="ui raised segment">`;
         for (var i = 0; i < 8; i++) {
           if (data[key][0][i] != null) {
-            content += `<p>${items[i]}:${data[key][0][i].replace(
-              " ★",
-              ""
-            )}</p>`;
+            content += `
+            <p style="clear: both;"><span style="float: left;"><b>${
+              items[i]
+            }</b></span>:<span style="float: right;">
+             ${data[key][0][i].replace(" ★", "")}
+            </span><p>`;
           }
         }
-        if (Object.keys(data).length != 1) content += `</div>`;
       } else {
-        if (Object.keys(data).length != 1)
-          content += `<div class="ui raised segment">`;
-        content += `<p>${key}:${data[key][0].res}<p>`;
-        if (Object.keys(data).length != 1) content += `</div>`;
+        content += `<p style="clear: both;"><span style="float: left;"><b>${key}</b></span>:<span style="float: right;">${data[key][0].res}</span><p>`;
       }
     }
   }
@@ -44,98 +41,101 @@ function formatter(type, data) {
   }
   return content;
 }
-function display_profs_data(profs_data, td, trIdx) {
+function fillin_popup(course_name, profs_data) {
+  $("#popup > .content").html(`
+      <div class="header">${course_name}</div>
+      <div class="description">${formatter("prof", profs_data)}</div>
+`);
+  $(`#popup`).removeClass("hidden").addClass("visible");
+}
+function make_btn(course_name, td, trIdx) {
+  $(td).removeClass("sm");
   td.innerHTML = `
             <div class="medium fluid ui button my-button" id="button${trIdx}">
              ${td.innerHTML}
             </div>
-            <div class="ui popup flowing bottom left transition hidden my-popup" id="popup${trIdx}">
-              <div class='header'></div><div class='content'>${formatter(
-                "prof",
-                profs_data
-              )}
-
-              </div>
-            </div>`;
+            `;
 
   $(`.button#button${trIdx}`)
     .mouseenter(function () {
-      var rect = $(`.button#button${trIdx}`)[0].getBoundingClientRect();
-      $(`.popup#popup${trIdx}`).removeClass("hidden").addClass("visible");
-      var rect_popup = $(`.popup#popup${trIdx}`)[0].getBoundingClientRect();
-      var popup_height = rect_popup.bottom - rect_popup.top;
-      console.log(popup_height);
-      $(`.popup#popup${trIdx}`)
-        .css(
-          "top",
-          rect.bottom -
-            (popup_height + 10 + rect.bottom - rect.top) / 2 -
-            document.body.getBoundingClientRect().top
-        )
-        .css("left", rect.left); //
+      // var rect = $(`.button#button${trIdx}`)[0].getBoundingClientRect();
+      // filter data
+      $(`#popup`).removeClass("visible").addClass("hidden");
+      var profs_data = {};
+      //.addClass("ui segment").css("position", "list-item");
+      var value = $(td).text().trim().replace("*", "<br>").split("<br>")[0];
+      if (value in window.json_data["urschool"]["professors"]) {
+        profs_data[value] = window.json_data["urschool"]["professors"][value];
+      } else {
+        profs_data[value] = [{ res: "找不到資料耶🥺" }];
+      }
+      console.log("👋:" + value);
+      fillin_popup(course_name, profs_data);
+      // setTimeout(function () {
+      //   if (
+      //     !$(`.button#button${trIdx}`).is(":hover") &&
+      //     !$(`#popup`).is(":hover")
+      //   )
+      //     $(`#popup`).removeClass("visible").addClass("hidden");
+      // }, 4000);
+      // var rect_popup = $(`.popup#popup${trIdx}`)[0].getBoundingClientRect();
+      // var popup_height = rect_popup.bottom - rect_popup.top;
+      // console.log(popup_height);
+      // $(`.popup#popup${trIdx}`)
+      //   .css(
+      //     "top",
+      //     rect.bottom -
+      //       (popup_height + 10 + rect.bottom - rect.top) / 2 -
+      //       document.body.getBoundingClientRect().top
+      //   )
+      //   .css("left", rect.left); //
     })
     .mouseleave(function () {
-      setTimeout(function () {
-        if (!$(".popup#popup${trIdx}").is(":hover"))
-          $(`.popup#popup${trIdx}`).removeClass("visible").addClass("hidden");
-      }, 50);
+      $("#popup").removeClass("visible").addClass("hidden");
     });
-  $(`.popup#popup${trIdx}`)
-    .mouseenter(function () {
-      var rect = $(`.button#button${trIdx}`)[0].getBoundingClientRect();
-      $(`.popup#popup${trIdx}`).removeClass("hidden").addClass("visible");
-      var rect_popup = $(`.popup#popup${trIdx}`)[0].getBoundingClientRect();
-      var popup_height = rect_popup.bottom - rect_popup.top;
-      console.log(popup_height);
-      $(`.popup#popup${trIdx}`)
-        .css(
-          "top",
-          rect.bottom -
-            (popup_height + 10 + rect.bottom - rect.top) / 2 -
-            document.body.getBoundingClientRect().top
-        )
-        .css("left", rect.left);
-    })
-    .mouseleave(function () {
-      if (!$(`.button#button${trIdx}`).is(":hover"))
-        $(`.popup#popup${trIdx}`).removeClass("visible").addClass("hidden");
-    });
+  // $(`.popup#popup${trIdx}`)
+  //   .mouseenter(function () {
+  //     // var rect = $(`.button#button${trIdx}`)[0].getBoundingClientRect();
+  //     // $(`.popup#popup${trIdx}`).removeClass("hidden").addClass("visible");
+  //     // var rect_popup = $(`.popup#popup${trIdx}`)[0].getBoundingClientRect();
+  //     var popup_height = rect_popup.bottom - rect_popup.top;
+  //     console.log(popup_height);
+  //     $(`.popup#popup${trIdx}`)
+  //       .css(
+  //         "top",
+  //         rect.bottom -
+  //           (popup_height + 10 + rect.bottom - rect.top) / 2 -
+  //           document.body.getBoundingClientRect().top
+  //       )
+  //       .css("left", rect.left);
+  //   })
+  //   .mouseleave(function () {
+  //     if (!$(`.button#button${trIdx}`).is(":hover"))
+  //       $(`.popup#popup${trIdx}`).removeClass("visible").addClass("hidden");
+  //   });
 }
-function filter_data(json_data) {
+function modify_html() {
   $("table.table > thead  > tr > th:nth-child(7)")[0].style.width = "11%";
   // var profs_set = new Set();
   $("table.table > tbody  > tr:visible").each(function (trIdx, tr) {
+    course_name = $(tr).find(".course_name").text().trim();
     $(tr)
       .find("td")
       .each(function (tdIdx, td) {
         if (tdIdx == 6) {
-          var profs_data = {};
-          $(td).removeClass("sm"); //.addClass("ui segment").css("position", "list-item");
-          td.innerHTML
-            .replace("*", "")
-            .split("<br>")
-            .forEach((value) => {
-              if (value in json_data["urschool"]["professors"]) {
-                profs_data[value] = json_data["urschool"]["professors"][value];
-              } else {
-                profs_data[value] = [{ res: "找不到資料耶🥺" }];
-              }
-              console.log("fetching:" + value);
-            });
-          display_profs_data(profs_data, td, trIdx);
+          make_btn(course_name, td, trIdx);
         }
       });
   });
 }
 
-var json_data = {};
 // 接收資料
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  console.log("received");
+  console.log("🐍");
   if (request.method == "response_data") {
     if (Object.keys(request.json_data).length != 0) {
-      json_data = request.json_data;
-      filter_data(json_data);
+      window.json_data = request.json_data;
+      modify_html();
       sendResponse({ complete: "ok" });
       $(".loading-btn").remove();
     } else {
@@ -151,6 +151,12 @@ if (
 ) {
   $("body").append(
     `<div class="loading-btn"><i class="fa fa-spinner fa-spin"></i></div>`
+  );
+  $("body").append(
+    `<div class="ui card" id="popup">
+    <div class="content">
+    </div>
+  </div>`
   );
   chrome.runtime.sendMessage({ method: "get_data" }, function (response) {
     if (response.complete == "ok") {
